d7709374c4b4eecb4033546c12883a4c49a775e6
Cyrus Li <70495046+Sothis-baka@users.noreply.github.com>
2021-05-18 11:22:19 -0400

    Update telephone.py

--- telephone.py
+++ telephone.py
@@ -1,26 +1,28 @@
 import json
+import time
+
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-authenticator = IAMAuthenticator("-LsDW7oVqIm2_wjDZeAcEnt5c3kmjqKoP7DWl90PX4BV")
+authenticator = IAMAuthenticator('-LsDW7oVqIm2_wjDZeAcEnt5c3kmjqKoP7DWl90PX4BV')
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
 )
 
-language_translator.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com/instances/2f7e501b-7dfb-4fc8-90e0-753de2e3af0e')
+language_translator.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com')
 
-
+# Input should be some complex sentence in case it will have some real changes, for example: how are you.
 sentence = input("Player 1, enter sentence: ")
 
-languages = ['en-es', 'es-fr', 'fr-en']
-translated_sentence = sentence
+languages = ["en-it", "it-de", "de-fr", "fr-en"]
 
-for language in languages:
+for i in range(4):
     translation = language_translator.translate(
-        text=translated_sentence,
-        model_id=language).get_result()
-    print(json.dumps(translation, indent=2, ensure_ascii=False))
-    translated_sentence = translation['translations'][0]['translation']
-
-print(translated_sentence)
+        text=sentence,
+        model_id=languages[i]).get_result()
+    result = json.dumps(translation, indent=2, ensure_ascii=False)
+    sentence = translation.get('translations')[0]['translation']
+    print("Player" + str(i+2) + ", languages: " + languages[i])
+    print(sentence)
+    time.sleep(1)
